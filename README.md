231b0097f7492cf9840e23b72e1101fc8580e6f6
Marco Lussetti <marco@marcolussetti.com>
2018-11-18 14:33:07 -0800

    chore(mnist_tools.nim, README) export to csv

--- README.md
+++ README.md
@@ -1,16 +1,46 @@
 # MNIST Tools
 
+## Installation
+
 ```sh
 nimble install https://github.com/marcolussetti/mnist_tools
 ```
 
-If files are in the local directory, just use mnistLoadDefaults(). Else refer to mnistLoad(imageFilePath: string, labelFilePath: string). Can load .gz files.
 
-Returns a sequence of tuples:
+## API
+
+This libraries provides 2 data types:
 ```nim
 type
-    MnistImage = tuple[label: int, image: seq[int]]
+    MnistImages* = seq[MnistImage]
+    MnistImage* = tuple[label: int, image: seq[int]]
 ```
+
+
+and 6 functions:
+
+```nim
+# Loads Training Data into a sequence of MnistImage
+# Downloads the files if not present in source directory
+proc mnistTrainingData*(sourceDir: string = ""): MnistImages
+
+# Loads Training Data into a sequence of MnistImage
+# Downloads the files if not present in source directory
+proc mnistTestData*(sourceDir: string = ""): MnistImages
+
+# Returns a string visualization of an image, showing items above the threshold
+proc mnistCoarseAsciiImage*(image: seq[int], cols: int = 28, threshold: int = 50): string
+
+# Allows for manually specifying paths to load
+proc mnistLoad*(imageFilePath: string, labelFilePath: string): MnistImages
+
+# Manually request download of MNIST files to specified directory
+proc mnistDownload*(outputDir: string = "")
+```
+
+## Examples
+
+### Loading data
 
 Provides a very coarse ascii print of the image as such:
 ```nim
@@ -20,4 +50,21 @@ let mnistTraining = mnistTrainingData()
 let mnistTest = mnistTestData()
 
 echo mnistTraining[7].image.mnistCoarseAsciiImage # Should print a "3"
+```
+
+### Exporting to CSV
+If all you need is the MNIST dataset as csvs, you can create a nim file with just a call to the function and then run it:
+
+```nim
+import mnist_tools
+
+mnistToCsv()
+```
+
+```sh
+nim c whateverfile.nim
+# *nix
+./whateverfile
+# Win
+whateverfile.exe
 ```
--- src/mnist_tools.nim
+++ src/mnist_tools.nim
@@ -1,9 +1,9 @@
-import streams, strutils, endians, os, sequtils, httpclient
+import streams, strutils, endians, os, sequtils, httpclient, sugar
 import zip/gzipfiles
 
 type
-    MnistImages = seq[MnistImage]
-    MnistImage = tuple[label: int, image: seq[int]]
+    MnistImages* = seq[MnistImage]
+    MnistImage* = tuple[label: int, image: seq[int]]
 
 const baseUrl = "http://yann.lecun.com/exdb/mnist/"
 const files = [
@@ -13,30 +13,27 @@ const files = [
     "t10k-labels-idx1-ubyte.gz"
 ]
 
-proc mnistDownload*(outputDir: string = "") =
-    var client = newHttpClient()
-    for file in files:
-        if not existsFile(outputDir & file):
-            echo("Downloading " & baseUrl & file & " ...")
-            client.downloadFile(baseUrl & file, outputDir & file)
-            assert existsFile(outputDir & file),
-                "For some reason file was not downloaded successfully"
 
+# Utilities
 proc readInt32BigEndian(input: Stream): int32 =
     var bytes = input.readInt32
     bigEndian32(addr result, addr bytes)
 
+
 proc readInt32BigEndianAsInt(input: Stream): int =
     return int(input.readInt32BigEndian)
+
 
 proc readIntFromIdx(input: Stream): int =
     return int(input.readChar)
 
+
+# Loader functions
 proc mnistLoadLabels(labelFileStream: Stream): seq[int] =
     # Extract metadata about file
     let magicNumber = int(labelFileStream.readInt32BigEndian)
-    assert magicNumber == 2049,
-        "Incorrect magic number provided! Is this a MNIST labels dataset?"
+    # assert magicNumber == 2049,
+    #     "Incorrect magic number provided! Is this a MNIST labels dataset?"
     let numberLabels = int(labelFileStream.readInt32BigEndian)
 
     var mnistLabels = newSeq[int]()
@@ -46,11 +43,12 @@ proc mnistLoadLabels(labelFileStream: Stream): seq[int] =
 
     return mnistLabels
 
+
 proc mnistLoadImages(imageFileStream: Stream): seq[seq[int]] =
     # Extract metadata about file
     let magicNumber = imageFileStream.readInt32BigEndianAsInt
-    assert magicNumber == 2051,
-        "Incorrect magic number provided! Is this a MNIST dataset?"
+    # assert magicNumber == 2051,
+    #     "Incorrect magic number provided! Is this a MNIST dataset?"
     let numberImages = imageFileStream.readInt32BigEndianAsInt
     let rowsInImage = imageFileStream.readInt32BigEndianAsInt
     let columnsInImage = imageFileStream.readInt32BigEndianAsInt
@@ -66,6 +64,7 @@ proc mnistLoadImages(imageFileStream: Stream): seq[seq[int]] =
 
     return mnistImages
 
+
 proc mnistCombine(labels: seq[int], images: seq[seq[int]]): MnistImages =
     var minLen = min(labels.len, images.len)
     newSeq(result, minLen)
@@ -73,8 +72,28 @@ proc mnistCombine(labels: seq[int], images: seq[seq[int]]): MnistImages =
         result[i] = (label: labels[i], image: images[i])
 
 
-proc mnistLoad*(imageFilePath: string, labelFilePath: string,
-        gunzipped: bool = true): MnistImages =
+proc mnistImagesToCsv(images: MnistImages): string =
+    let header = lc[ [$i, $j].join("_") | (i <- 0..<28, j <- 0..<28), string]
+    result &= "Label," & header.join(",")
+    result &= "\n"
+
+    for image in images:
+        result &= $image[0] & "," & image[1].join(",")
+        result &= "\n"
+
+
+# Exported functions
+proc mnistDownload*(outputDir: string = "") =
+    var client = newHttpClient()
+    for file in files:
+        if not existsFile(outputDir & file):
+            echo("Downloading " & baseUrl & file & " ...")
+            client.downloadFile(baseUrl & file, outputDir & file)
+            assert existsFile(outputDir & file),
+                "For some reason file was not downloaded successfully"
+
+
+proc mnistLoad*(imageFilePath: string, labelFilePath: string): MnistImages =
     if not existsFile(labelFilePath):
         raise newException(IOError,
                 "Label path provided does not exist or is not a file.")
@@ -82,14 +101,8 @@ proc mnistLoad*(imageFilePath: string, labelFilePath: string,
         raise newException(IOError,
                 "Path provided does not exist or is not a file.")
 
-    var imageFileStream, labelFileStream: Stream
-
-    if gunzipped:
-        imageFileStream = newGzFileStream(imageFilePath)
-        labelFileStream = newGzFileStream(labelFilePath)
-    else:
-        imageFileStream = newFileStream(imageFilePath, fmRead)
-        labelFileStream = newFileStream(labelFilePath, fmRead)
+    var imageFileStream = newGzFileStream(imageFilePath, fmRead)
+    var labelFileStream = newGzFileStream(labelFilePath, fmRead)
 
     let images = mnistLoadImages(imageFileStream)
     let labels = mnistLoadLabels(labelFileStream)
@@ -102,24 +115,43 @@ proc mnistLoad*(imageFilePath: string, labelFilePath: string,
     return mnistCombine(labels, images)
 
 
-proc mnistTrainingData*(sourceDir: string = ""): MnistImages =
+proc mnistTrainingData*(sourceDir = ""): MnistImages =
     if not existsFile(sourceDir & files[0]) or not existsFile(
             sourceDir & files[1]):
         mnistDownload(sourceDir)
     return mnistLoad(sourceDir & files[0], sourceDir & files[1])
 
 
-proc mnistTestData*(sourceDir: string = ""): MnistImages =
+proc mnistTestData*(sourceDir = ""): MnistImages =
     if not existsFile(sourceDir & files[2]) or not existsFile(
             sourceDir & files[3]):
         mnistDownload(sourceDir)
     return mnistLoad(sourceDir & files[2], sourceDir & files[3])
 
 
-proc mnistCoarseAsciiImage*(image: seq[int], cols: int = 28,
-        threshold: int = 50): string =
+proc mnistCoarseAsciiImage*(image: seq[int], cols = 28,
+        threshold = 50): string =
     for i in 0..<image.len:
         result &= (if image[i] > threshold: "██" else: "  ")
         if i mod cols == 0: result &= "\n"
 
     result &= "\n"
+
+
+proc mnistToCsv*(sourceDir = "", outputDir = "") =
+    echo "Loading Training Data. This might take a while..."
+    let trainingString = mnistImagesToCsv(mnistTrainingData(sourceDir))
+
+    echo "Converting Training data to CSV. This might take a while..."
+    var trainingStream = newFileStream(outputDir & "mnist-training.csv",
+            fmWrite)
+    trainingStream.write(trainingString)
+    trainingStream.close
+
+    echo "Loading Test Data. This might take a while..."
+    let testString = mnistImagesToCsv(mnistTestData(sourceDir))
+    echo "Converting Training data to CSV. This might take a while..."
+    var testStream = newFileStream(outputDir & "mnist-test.csv",
+            fmWrite)
+    testStream.write(testString)
+    testStream.close
